b269b3bb60e9cf703b97417cd498626527102e00
Cory Hayes <corymhayes@gmail.com>
2020-06-03 16:34:38 -0500

    fixed fonts

--- pages/_app.js
+++ pages/_app.js
@@ -4,24 +4,24 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 
   /*  USE FOR DEVELOPMENT */
-  // @font-face{
-  //   font-family: 'Industry';
-  //   src: url('/static/fonts/Industry-Bold.woff2') format('woff2'),
-  //        url('/static/fonts/Industry-Bold.woff') format('woff'),
-  //        url('/static/fonts/Industry-Bold.ttf') format('truetype');
-  //   font-weight: bold;
-  //   font-style: normal;
-  // }
+  /* @font-face{
+    font-family: 'Industry';
+    src: url('/static/fonts/Industry-Bold.woff2') format('woff2'),
+         url('/static/fonts/Industry-Bold.woff') format('woff'),
+         url('/static/fonts/Industry-Bold.ttf') format('truetype');
+    font-weight: bold;
+    font-style: normal;
+  }
 
-  // @font-face{
-  //   font-family: 'Industry';
-  //   src: url('/static/fonts/Industry-Medium.woff2') format('woff2'),
-  //        url('/static/fonts/Industry-Medium.woff') format('woff'),
-  //        url('/static/fonts/Industry-Medium.ttf') format('truetype'),
-  //        url('/static/fonts/Industry-Medium.svg#Industry-Medium') format('svg');
-  //   font-weight: 500;
-  //   font-style: normal;
-  // }
+  @font-face{
+    font-family: 'Industry';
+    src: url('/static/fonts/Industry-Medium.woff2') format('woff2'),
+         url('/static/fonts/Industry-Medium.woff') format('woff'),
+         url('/static/fonts/Industry-Medium.ttf') format('truetype'),
+         url('/static/fonts/Industry-Medium.svg#Industry-Medium') format('svg');
+    font-weight: 500;
+    font-style: normal;
+  } */
 
   /*  USE FOR PRODUCTION */  
   @font-face{
